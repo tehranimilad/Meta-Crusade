16f6866ac382b8318087c0a57849818b24863e7c
Milad Tehrani <tehranimilad92@gmail.com>
2023-02-15 11:23:06 -0800

    creating initial file structure

---
+++ frontend/src/pages/HomePage/index.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+const HomePage = () => {
+    return(
+        <div>
+            <h1>Welcome to Meta Crusade</h1>
+        </div>
+    )
+}
+
+export default HomePage
