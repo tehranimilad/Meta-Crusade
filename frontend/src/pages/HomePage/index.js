e1afc106ab282036be883ca4bf9857df4fd82a62
Milad Tehrani <tehranimilad92@gmail.com>
2023-03-09 16:22:41 -0800

    added navbar

--- frontend/src/pages/HomePage/index.js
+++ frontend/src/pages/HomePage/index.js
@@ -1,10 +1,13 @@
 import Image from 'react-bootstrap/Image'
 import metacrusade from './assets/Meta-Crusade.png';
 import React from 'react';
+import Nav from 'react-bootstrap/Nav'
 
 const HomePage = () => {
     return(
         <div>
+            <Nav>
+            </Nav>
             <Image className="homeImage" src={metacrusade} ></Image>
         </div>
     )
