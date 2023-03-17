b291d0e22c3faa865de6d123cca06804d26dd8ab
Milad Tehrani <tehranimilad92@gmail.com>
2023-03-17 14:27:41 -0500

    imported route for new learn more page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Routes, Route } from "react-router-dom";
 import HomePage from './pages/HomePage';
 import LandingPage from './pages/LandingPage';
+import LearnMore from './pages/LearnMore';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/home" element={<HomePage />} />
+        <Route path="/learnmore" element={<LearnMore />} />
       </Routes>
     </main>
 
--- frontend/src/pages/HomePage/index.js
+++ frontend/src/pages/HomePage/index.js
@@ -2,6 +2,8 @@ import Image from 'react-bootstrap/Image'
 import metacrusade from './assets/Meta-Crusade.png';
 import React from 'react';
 import Nav from 'react-bootstrap/Nav'
+import { Link, useNavigate } from "react-router-dom";
+
 
 const HomePage = () => {
     return(
@@ -10,6 +12,7 @@ const HomePage = () => {
             </Nav>
             <Image className="homeImage" src={metacrusade} ></Image>
             <h1>Welcome to meta</h1>
+            <Link to="/learnmore">Learn More About Us</Link>
         </div>
     )
 }
