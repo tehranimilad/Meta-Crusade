8c9199dc444094fa49333c782ad3560853306f20
Milad Tehrani <tehranimilad92@gmail.com>
2023-03-23 16:58:31 -0400

    adding css for animejs button animation

--- frontend/src/pages/HomePage/index.js
+++ frontend/src/pages/HomePage/index.js
@@ -3,6 +3,7 @@ import metacrusade from './assets/Meta-Crusade.png';
 import React from 'react';
 import Nav from 'react-bootstrap/Nav'
 import { Link } from "react-router-dom";
+import Button from 'react-bootstrap/Button'
 import './HomePage.css'
 
 
@@ -11,6 +12,7 @@ const HomePage = () => {
         <div className="homePageDiv">
             <Nav>
             </Nav>
+            <Button>Back</Button>
             <Image className="homeImage" src={metacrusade} ></Image>
             <h1 className="homeContent">Welcome to Meta Crusade</h1>
             <Link className="homeLink" to="/learnmore">Learn More</Link>
