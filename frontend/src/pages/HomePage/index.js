cf3c04bcd71d29c77f3381057e04a3bf241957d4
Milad Tehrani <tehranimilad92@gmail.com>
2023-03-23 17:02:45 -0400

    nuilding out animejs functionaility

--- frontend/src/pages/HomePage/index.js
+++ frontend/src/pages/HomePage/index.js
@@ -9,14 +9,30 @@ import './HomePage.css'
 
 const HomePage = () => {
     return(
-        <div className="homePageDiv">
-            <Nav>
-            </Nav>
-            <Button>Back</Button>
-            <Image className="homeImage" src={metacrusade} ></Image>
-            <h1 className="homeContent">Welcome to Meta Crusade</h1>
-            <Link className="homeLink" to="/learnmore">Learn More</Link>
-        </div>
+
+
+<main>
+  <div class="button">
+    <div class="text">Submit</div>
+  </div>
+  <div class="progress-bar"></div>
+  <svg x="0px" y="0px"
+	 viewBox="0 0 25 30" style="enable-background:new 0 0 25 30;">
+    <path class="check" className="st0" d="M2,19.2C5.9,23.6,9.4,28,9.4,28L23,2"/>
+  </svg>
+</main>
+
+
+
+
+        // <div className="homePageDiv">
+        //     <Nav>
+        //     </Nav>
+        //     <Button>Back</Button>
+        //     <Image className="homeImage" src={metacrusade} ></Image>
+        //     <h1 className="homeContent">Welcome to Meta Crusade</h1>
+        //     <Link className="homeLink" to="/learnmore">Learn More</Link>
+        // </div>
     )
 }
 
