968ceebd1f065864cddf8cb0b80f2f72a3f54b37
Milad Tehrani <tehranimilad92@gmail.com>
2023-03-24 08:53:00 -0700

    optimizing landing page image

--- frontend/src/components/Nav/index.js
+++ frontend/src/components/Nav/index.js
@@ -5,10 +5,8 @@ import { Link, useNavigate } from "react-router-dom";
 
 function Nav() {
     return (
-      <nav className="navbar mavbar-expand-lg navbar-light bg-light">
-        <div className="navbar-header">
+      <nav className="navbar mavbar-expand-lg bg-light">
           <a className="navbar-brand" href="/">Meta Crusade</a>
-        </div>
         <Link to="/home" className="nav-link">Home</Link>
         <Link to="/learnmore" className="nav-link">Learn More</Link>
     
