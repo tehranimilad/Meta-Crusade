7c17ea2b3244f5560e36fe4314485325ae936635
Milad Tehrani <tehranimilad92@gmail.com>
2023-03-23 19:11:05 -0600

    attempting to reformat the learn more page

--- frontend/src/pages/LearnMore/index.js
+++ frontend/src/pages/LearnMore/index.js
@@ -12,11 +12,11 @@ const LearnMore = () => {
             <div>
             <Card className="learnMoreDiv">
                 <h1>Here is more information about us:</h1>
-                <ul> Team Members
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-                <li>4</li>
+                <ul className="infoUl"> Team Members
+                <li className="infoUl">Meezy</li>
+                <li className="infoUl">OxR0amPal</li>
+                <li className="infoUl">3</li>
+                <li className="infoUl">4</li>
             </ul>
             <ul> Games we're creating
                 <li>Idea 1</li>
