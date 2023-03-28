ceaa5d6fc2439c247032c19650e325f5ffcaff64
Milad Tehrani <tehranimilad92@gmail.com>
2023-03-28 14:43:17 -0700

    updating nav links

--- frontend/src/components/Nav/index.js
+++ frontend/src/components/Nav/index.js
@@ -5,8 +5,11 @@ import { Link, useNavigate } from "react-router-dom";
 
 function Nav() {
     return (
-      <nav className="navbar mavbar-expand-lg bg-light">
-          <a className="navbar-brand" href="/">Meta Crusade</a>
+
+      <nav className="navbar mavbar-expand-lg navbar-light bg-light">
+      <div className="navbar-header">
+      <a className="navbar-brand" href="/">Meta Crusade</a>
+      </div>
         <Link to="/home" className="nav-link">Home</Link>
         <Link to="/learnmore" className="nav-link">Learn More</Link>
     
